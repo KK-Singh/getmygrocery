c211c0c23a28570de45ac61852a6d21ab5dea5ae
KK-Singh <35195391+KK-Singh@users.noreply.github.com>
2022-11-19 09:05:27 -0500

    Updates

---
+++ precache-manifest.1d3405b491754c1a417f1ca3f252da00.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f90a16b767c97d3b7559e0b585edfa7",
+    "url": "/getmygrocery/index.html"
+  },
+  {
+    "revision": "a561a02af9406945a2ed",
+    "url": "/getmygrocery/static/css/main.d6eaa1d2.chunk.css"
+  },
+  {
+    "revision": "125380dab3af9d6aa02a",
+    "url": "/getmygrocery/static/js/2.fd20404d.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/getmygrocery/static/js/2.fd20404d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a561a02af9406945a2ed",
+    "url": "/getmygrocery/static/js/main.b67597f8.chunk.js"
+  },
+  {
+    "revision": "2deb67073a7933a5ce94",
+    "url": "/getmygrocery/static/js/runtime-main.34c4f6ac.js"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/getmygrocery/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/getmygrocery/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "7fbd0f236518f5b0975edfc57d0cd4cc",
+    "url": "/getmygrocery/static/media/profile.7fbd0f23.webp"
+  },
+  {
+    "revision": "36c82bc1d31531915eebeb7315147afd",
+    "url": "/getmygrocery/static/media/slide1.36c82bc1.webp"
+  },
+  {
+    "revision": "96650089b6c49414255bad6a628f42a3",
+    "url": "/getmygrocery/static/media/slide2.96650089.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "/getmygrocery/static/media/slide3.8e5b87b6.webp"
+  },
+  {
+    "revision": "1792c56174835b8ba092482aede5b5d6",
+    "url": "/getmygrocery/static/media/slide4.1792c561.webp"
+  },
+  {
+    "revision": "22b0e882cbd4b473fbcfce02ee4536a7",
+    "url": "/getmygrocery/static/media/slide5.22b0e882.webp"
+  },
+  {
+    "revision": "ff2e1abe55a7eca0bd42ff83eb6197dc",
+    "url": "/getmygrocery/static/media/store1.ff2e1abe.jpg"
+  },
+  {
+    "revision": "3aba35b525495ec0b2fd2660971c67a1",
+    "url": "/getmygrocery/static/media/store4.3aba35b5.jpg"
+  },
+  {
+    "revision": "477c8f44985734c36caefe1f18e4a33a",
+    "url": "/getmygrocery/static/media/store5.477c8f44.jpg"
+  }
+]);
